1608672fb57cb9eaf843e60f8860bf3644f3e839
wxhbts <519165351@qq.com>
2025-08-13 03:40:15 +0800

    Update [[default]].js

--- functions/proxy/[[default]].js
+++ functions/proxy/[[default]].js
@@ -1,7 +1,8 @@
 // functions/proxy.js
-export async function onRequest({ request }) {
+export async function onRequest({ request， waitUntil }) {
   const url = new URL(request.url);
   const targetUrl = url.searchParams.get('url');
+	 const cookie = url.searchParams.get('cookie');
 
   if (!targetUrl) {
     return new Response('Missing "url" parameter', { status: 400 });
@@ -14,12 +15,23 @@ export async function onRequest({ request }) {
     return new Response('Invalid "url" parameter', { status: 400 });
   }
 
-	  try {
-    const response = await fetch(targetUrl);
-    if (!response.ok) {
+  const headers = new Headers();
+ 
+    headers.set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36');
+    headers.set('cookie'， cookie);
+  
 
+  try {
+    const response = await fetch(targetUrl, {
+      headers: headers，  // 传递原始请求头 (可选)
+    });
+
+    if (!response.ok) {
+      // 代理错误状态码
       return new Response(response.statusText, { status: response.status });
     }
+
+     // 克隆响应头
     const newHeaders = new Headers(response.headers);   //复制原始header
     newHeaders.set('Access-Control-Allow-Origin', '*'); // 设置 CORS
     newHeaders.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
@@ -30,6 +42,7 @@ export async function onRequest({ request }) {
     newHeaders.set('Pragma', 'no-cache');
     newHeaders.set('Expires', '0');
 
+    // 返回代理的响应
     return new Response(response.body, {
       status: response.status,
       headers: newHeaders,
