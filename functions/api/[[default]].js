0e9dc1733c106734ee2dc6389ed0eebc88aa94b0
wxhbts <519165351@qq.com>
2026-01-11 05:44:14 +0800

    Update [[default]].js

--- functions/api/[[default]].js
+++ functions/api/[[default]].js
@@ -35,6 +35,21 @@ export async function onRequest(context) {
       }
     }
 
+    // 设置必要的请求头 (可选，如果需要覆盖目标站点的 Host)
+    const originUrl = new URL(targetUrlParam);
+    headers.set('Host', originUrl.host);
+     if (url.searchParams.get('name') === 'pandalive') {
+      headers.set('Origin', 'https://www.pandalive.co.kr');
+    }
+    if (url.searchParams.get('name') === 'twitch') {
+      headers.set('Origin', 'https://www.twitch.tv');
+      headers.set('referer', 'https://www.twitch.tv/');
+    }
+    if (url.searchParams.get('name') === 'ttinglive') {
+      headers.set('Origin', 'https://www.ttinglive.com');
+      headers.set('referer', 'https://www.ttinglive.com/');
+    }
+    // headers.set('Origin', originUrl.origin); // 如果需要设置 Origin，请取消注释
 
     // 判断是否有Cookie，并打印日志
     const cookies = request.headers.get('cookie');
